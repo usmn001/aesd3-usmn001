64368c7971e5c998a0ff48daf14929c31a68b9dd
musman <m_usman010@yahoo.com>
2026-05-12 22:12:16 +0100

    Fixed:init script again

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,12 +2,12 @@
 
 case "$1" in
     start)
-        echo "Starting simple server"
-        start-stop-daemon -S -n simpleserver -a /usr/bin/simpleserver -- -d
+        echo "Starting simple server aesdsocket"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping simple server"
-        start-stop-daemon -K -n simpleserver
+        start-stop-daemon -K -n aesdsocket
         ;;
     *)
         echo "Usage : $0 {start|stop}"
