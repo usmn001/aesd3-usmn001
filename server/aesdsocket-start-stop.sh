6814afb9bcd1c8d28dbdff441db1b39eb61d2672
musman <m_usman010@yahoo.com>
2026-05-12 21:45:30 +0100

    Correct server init script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+
 case "$1" in
     start)
-     echo "Starting simple server"
-     start-stop-daemon -S -n simpleserver -a /etc/init.d/S99aesdsocket -d
-     ;;
-     stop)
-     echo "Stopping simple server"
-     start-stop-daemon -K -n simpleserver
-     ;;
-     *)
-     echo "Usage : $0 {start|stop}"
-    exit 1
-easc
+        echo "Starting simple server"
+        start-stop-daemon -S -n simpleserver -a /usr/bin/simpleserver -d
+        ;;
+    stop)
+        echo "Stopping simple server"
+        start-stop-daemon -K -n simpleserver
+        ;;
+    *)
+        echo "Usage : $0 {start|stop}"
+        exit 1
+        ;;
+esac
+
 exit 0     
